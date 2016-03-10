1214c199d831260f5464cfddcbfec9ae0a81b36a
Daniel S <standeo+github@gmail.com>
2016-03-10 11:01:22 +1100

    Initial commit

---
+++ src/InstaHuskee.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+module InstaHuskee (getAuthToken) where
+
+import           Data.Text
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS (tlsManagerSettings)
+import           Control.Monad.Trans.Resource
+import           Control.Monad.IO.Class
+import           Instagram
+
+-- To be parametrised/read from the environment
+
+code :: Text
+code = pack "YOUR_CODE"
+
+redirectUri :: RedirectUri
+redirectUri = pack "http://example.com/auth/ig"
+
+credentials :: Credentials
+credentials = Credentials (pack "CLIENT_ID") (pack "CLIENT_SECRET")
+
+-- Authenticated actions --
+
+getAuthToken :: IO OAuthToken
+getAuthToken = runIGAction $ getUserAccessTokenURL2 redirectUri code
+
+-- Mechanics --
+
+runIGAction :: InstagramT (ResourceT IO) a -> IO a
+runIGAction = runResourceT . runInstagramFn
+
+runInstagramFn :: (MonadBaseControl IO m, MonadResource m) => InstagramT m b -> m b
+runInstagramFn igAction = do
+  manager <- liftIO $ newManager tlsManagerSettings
+  runInstagramT credentials manager igAction
